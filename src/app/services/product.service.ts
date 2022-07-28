aa153f072272e79f9b084de3d0862f6ab250a033
osva-developer <oswaldomoralesperez94@gmail.com>
2022-07-28 17:30:44 -0500

    sidebar was added bu it is not working

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -12,9 +12,11 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    // need to build url based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     return this.httpClient
-      .get<GetResponse>(this.baseUrl)
+      .get<GetResponse>(searchUrl)
       .pipe(map((response) => response._embedded.products));
   }
 }
