b241641ef005bf4e590864d627430fce3ee5a89c
osva-developer <oswaldomoralesperez94@gmail.com>
2022-08-31 18:29:28 -0500

    cart Status component works dinamically

--- src/app/components/cart-status/cart-status.component.ts
+++ src/app/components/cart-status/cart-status.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   templateUrl: './cart-status.component.html',
-  styleUrls: ['./cart-status.component.css']
+  styleUrls: ['./cart-status.component.css'],
 })
 export class CartStatusComponent implements OnInit {
-
-  constructor() { }
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.updateCartStatus();
   }
+  updateCartStatus() {
+    //subscribe to the cart totlprice
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
 
+    //subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
 }
