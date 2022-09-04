83605e1c3059b6adb97f0063eedd97e56db921e9
osva-developer <oswaldomoralesperez94@gmail.com>
2022-09-04 13:12:00 -0500

    https was added **Angular SB

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa6dujonjTbz5KBR5d7',
     issuer: 'https://dev-41844751.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
